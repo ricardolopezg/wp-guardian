7f67434c37b128ee0ae816b88fcb3ff99dace4ff
ph47s74x <32150884+ph47s74x@users.noreply.github.com>
2020-01-10 15:59:25 -0500

    fix markdown formatting in jsontomd

--- jsontomd.py
+++ jsontomd.py
@@ -17,7 +17,6 @@ def getcvelist(item):
         if 'cve' in vuln['references']:
             for cve in vuln['references']['cve']:
                 cvelist.append(cve)
-                # print("ite")
     return cvelist
 
 def return_report_id(datastore):
@@ -26,29 +25,29 @@ def return_report_id(datastore):
 
 vulnslist = getcvelist(datastore)
 sortedvulnslist = sortbycvss(vulnslist)
-# print(sortedvulnslist)
 
 def print_sorted_vulns_list(sortedvulnslist):
     alldata = ''
     for information in sortedvulnslist:
-        # alldata += '\n' +'CVE-'+ information['id'] +'\n' + '\n' + information['description'] + '\n' +information['href']
         alldata += f'\n --- \n ### **{information["id"]}**\n\n'
         alldata += f'**CVSS Score:** \n\n {information["cvss"]["score"]}\n\n'
         alldata += f'**CVSS Vector:** \n\n {information["cvss"]["vector"]}\n\n'
         alldata += f'>{information["description"]}\n\n'
         alldata += f'References:\n\n>{information["href"]}\n'
-        # print(alldata)
     return alldata
 
-# print_sorted_vulns_list(sortedvulnslist)
 md_data= ''
-md_data += f'report #{return_report_id(datastore)}\n\n'
+md_data += f'report #'
+md_data += f' {return_report_id(datastore)}\n\n'
 md_data += f'# {datastore[return_report_id(datastore)]["report title"]}\n\n'
 md_data += f'### {datastore[return_report_id(datastore)]["date"]}\n\n' 
-md_data += f'## Domain\n\n{datastore[return_report_id(datastore)]["domain"]}\n\n' 
-md_data += f'## Summary \n\n\n > {datastore[return_report_id(datastore)]["summary"]}\n\n' 
+md_data += f'## Domain\n\n'
+md_data += f'{datastore[return_report_id(datastore)]["domain"]}\n\n' 
+md_data += f'## Summary \n\n' 
+md_data += f'> {datastore[return_report_id(datastore)]["summary"]}\n\n' 
 md_data += f'## Vulnerabilities  \n\n' 
-md_data += f'### *Wordpress Version: {datastore[return_report_id(datastore)]["assets"]["wordpress"]["version"]}*\n\n' 
+md_data += f'### *Wordpress Version: '
+md_data += f'{datastore[return_report_id(datastore)]["assets"]["wordpress"]["version"]}*\n\n'
 md_data += f'{print_sorted_vulns_list(sortedvulnslist)}\n\n' 
 
 path_folder = 'C:/Users/anon/Desktop/wp-guardian/'
