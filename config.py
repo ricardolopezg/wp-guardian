14cb44c11710d35e2dad78b47aa04035549f662d
Ricardo Lopez <ricardo.lopez.a@gmail.com>
2020-01-12 14:52:19 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# API KEY https://wpvulndb.com/api (50 free daily requests per token)
+wpvulndb_api_key = '<YOUR API KEY>'
+
+# API KEY https://vulners.com/ (1,000 free monthly requests per token)
+vulners_api_key = '<YOUR API KEY>'
