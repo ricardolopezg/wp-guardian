ca7be507dd30268fbecc02908308e48f873d4836
ph47s74x <32150884+ph47s74x@users.noreply.github.com>
2020-01-11 23:18:34 -0500

    added ascii art to wp_guardian and added pagefuzzer info to md

--- page_fuzzer.py
+++ page_fuzzer.py
@@ -8,21 +8,21 @@ def getserverversion(url):
 def fuzzupdatepage(url):
     r = requests.get(url+'/updates.php')
     if r.status_code == 404:
-        updatesphpnotfound = 'updates.php not avialible. Good Job!'
-        print(updatesphpnotfound)
+        updatesphpnotfound = f' - [x] updates.php is not accessible'
+        # print(updatesphpnotfound)
         return updatesphpnotfound
     if r.status_code == 200:
-        updatesphpfound = 'updates.php is availible. This is not secure'
-        print(updatesphpfound)
+        updatesphpfound = f'updates.php is accessible. This is not secure'
+        # print(updatesphpfound)
         return updatesphpfound
 
 def fuzzinstallpage(url):
     r = requests.get(url+'/install.php')
     if r.status_code == 404:
-        updatesphpnotfound = 'install.php not avialible. Good Job!'
-        print(updatesphpnotfound)
+        updatesphpnotfound = f' - [x] install.php is not accessible'
+        # print(updatesphpnotfound)
         return updatesphpnotfound
     if r.status_code == 200:
-        updatesphpfound = 'install.php is availible. This is not secure'
-        print(updatesphpfound)
+        updatesphpfound = f'install.php is accessible. This is not secure'
+        # print(updatesphpfound)
         return updatesphpfound
--- wp_guardian.py
+++ wp_guardian.py
@@ -4,16 +4,28 @@ import sys
 import api_combined
 import wp_version_scanner as wvs
 import jsontomd
-
+import page_fuzzer
 def main():
     url = sys.argv[1]
     if len(url) < 1:
         exit('Usage:wp_guardian.py http(s)://www.example.com(/wordpress)')
+    print('\n')    
+    print('██╗    ██╗██████╗      ██████╗ ██╗   ██╗ █████╗ ██████╗ ██████╗ ██╗ █████╗ ███╗   ██╗')
+    print('██║    ██║██╔══██╗    ██╔════╝ ██║   ██║██╔══██╗██╔══██╗██╔══██╗██║██╔══██╗████╗  ██║')
+    print('██║ █╗ ██║██████╔╝    ██║  ███╗██║   ██║███████║██████╔╝██║  ██║██║███████║██╔██╗ ██║')
+    print('██║███╗██║██╔═══╝     ██║   ██║██║   ██║██╔══██║██╔══██╗██║  ██║██║██╔══██║██║╚██╗██║')
+    print('╚███╔███╔╝██║         ╚██████╔╝╚██████╔╝██║  ██║██║  ██║██████╔╝██║██║  ██║██║ ╚████║')
+    print(' ╚══╝╚══╝ ╚═╝          ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝')
+    print('IS BUILDING YOUR REPORT. PLEASE WAIT...')
+                                                                                     
 
     soup = wvs.url_input(url)
     wp_version = wvs.wp_version_finder(soup)
     filename = api_combined.report_builder(wp_version, url)
-    jsontomd.jsontomd(filename)
+    server_version = page_fuzzer.getserverversion(url)
+    update_page = page_fuzzer.fuzzupdatepage(url)
+    install_page = page_fuzzer.fuzzinstallpage(url)
+    jsontomd.jsontomd(filename,server_version,install_page,update_page)
     print("WP GUARDIAN SCAN COMPLETE! Check your reports directory.")
 
 if __name__ == "__main__":
